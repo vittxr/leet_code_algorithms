d57f950635d3a495b4eca01ed5e9b60b2f36321c
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-20 10:00:37 -0300

    feat: add alternative solution

--- reverse-interger/solution.py
+++ reverse-interger/solution.py
@@ -21,3 +21,24 @@ class Solution:
             return 0
 
         return res
+
+# Alternative solution: 
+"""
+class Solution:
+    def newMod(self, a, b):
+        res = a % b
+        return res if not res else res - b if a < 0 else res
+
+    def reverse(self, x: int) -> int:
+        new_value = 0
+
+        while x:
+            r = self.newMod(x, 10)
+            new_value = new_value * 10 + r
+            x = int(x / 10)
+
+            if new_value > max_value or new_value < min_value:
+                return 0
+
+        return new_value
+"""
